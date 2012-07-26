8094cc6410b7dc2db452eb19cc9274fda1b2d6a2
Felipe Leme <felipeal@android.com>
2012-07-25 19:49:10 -0700

    Fix for issue #4: dynamically sets the name of the broadcast receiver class

--- gcm-client/src/com/google/android/gcm/GCMBroadcastReceiver.java
+++ gcm-client/src/com/google/android/gcm/GCMBroadcastReceiver.java
@@ -36,10 +36,19 @@ import android.util.Log;
 public class GCMBroadcastReceiver extends BroadcastReceiver {
 
     private static final String TAG = "GCMBroadcastReceiver";
+    private static boolean mReceiverSet = false;
 
     @Override
     public final void onReceive(Context context, Intent intent) {
         Log.v(TAG, "onReceive: " + intent.getAction());
+        // do a one-time check if app is using a custom GCMBroadcastReceiver
+        if (!mReceiverSet) {
+            mReceiverSet = true;
+            String myClass = getClass().getName();
+            if (!myClass.equals(GCMBroadcastReceiver.class.getName())) {
+                GCMRegistrar.setRetryReceiverClassName(myClass);
+            }
+        }
         String className = getGCMIntentServiceClassName(context);
         Log.v(TAG, "GCM IntentService class: " + className);
         // Delegates to the application-specific intent service.
@@ -51,9 +60,16 @@ public class GCMBroadcastReceiver extends BroadcastReceiver {
      * Gets the class name of the intent service that will handle GCM messages.
      */
     protected String getGCMIntentServiceClassName(Context context) {
+        return getDefaultIntentServiceClassName(context);
+    }
+
+    /**
+     * Gets the default class name of the intent service that will handle GCM
+     * messages.
+     */
+    static final String getDefaultIntentServiceClassName(Context context) {
         String className = context.getPackageName() +
                 DEFAULT_INTENT_SERVICE_CLASS_NAME;
         return className;
     }
-
 }
