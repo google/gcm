d10c3ab6cd7fe3fe56c4e9cfd0982247c6023aa0
Arthur Thompson <arthurthompson@google.com>
2016-01-15 14:25:52 -0800

    Test PRs

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 tagLen=${#TRAVIS_TAG}
-if [ $tagLen -gt 0 ]; then
-  echo "tagged so deploy"
-  openssl aes-256-cbc -K $encrypted_8864fe5d711d_key -iv $encrypted_8864fe5d711d_iv -in sign.key.enc -out sign.key -d
-  gpg --import sign.key
-  ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib deploy
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+  if [ $tagLen -gt 0 ]; then
+    echo "tagged so deploy"
+    openssl aes-256-cbc -K $encrypted_8864fe5d711d_key -iv $encrypted_8864fe5d711d_iv -in sign.key.enc -out sign.key -d
+    gpg --import sign.key
+    ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib deploy
+  else
+    echo "not tagged so stage"
+    ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib stage
+  fi
 else
-  echo "not tagged so stage"
-  ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib stage
+  echo "PR so test"
+  ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib tests
 fi
