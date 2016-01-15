babd7653051ff15917f9e00490beb84145a74f40
Arthur Thompson <arthurthompson@google.com>
2016-01-15 12:19:33 -0800

    Decrypt and install gpg keys only when deploying

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 tagLen=${#TRAVIS_TAG}
 if [ $tagLen -gt 0 ]; then
   echo "tagged so deploy"
+  openssl aes-256-cbc -K $encrypted_8864fe5d711d_key -iv $encrypted_8864fe5d711d_iv -in sign.key.enc -out sign.key -d
+  gpg --import sign.key
   ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib deploy
 else
   echo "not tagged so stage"
